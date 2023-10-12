5044bcb3e542c2ddef6ad0b08b5540273fc27d4f
irtaza <muhammadirtaza560@gmail.com>
2023-10-12 11:48:24 +0400

    exersice2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -0,0 +1,6 @@
+# Define the quote and its author
+quote = "You must be the change you wish to see in the world."
+author = "Mahatma Gandhi"
+
+# Print the quote and its author
+print(f'{author} once said, "{quote}"')
