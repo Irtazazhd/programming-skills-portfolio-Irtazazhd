f2a1b60c580f71240e807a387c312257568454bc
irtaza <muhammadirtaza560@gmail.com>
2023-10-12 11:51:13 +0400

    exersice4.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise4.py
@@ -0,0 +1,9 @@
+# Define your favorite number
+favorite_number = 7
+
+# Create a message to reveal your favorite number
+message = f"My favorite number is {favorite_number}."
+
+# Print the message
+print(message)
+
