d44451d6a50c3d327c56e7bbca531d4780af703d
irtaza <muhammadirtaza560@gmail.com>
2023-10-12 11:47:09 +0400

    exersice1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise1.py
@@ -0,0 +1,7 @@
+# Assign a message to a variable
+message = "Hello, World!"
+print("Original Message:", message)
+
+# Change the value of the variable to a new message
+message = "This is a new message."
+print("New Message:", message)
