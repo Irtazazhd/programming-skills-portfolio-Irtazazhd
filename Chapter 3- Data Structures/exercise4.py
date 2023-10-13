ef0d1c12da61c86386d098c75da4ba4553dc8d14
irtaza <muhammadirtaza560@gmail.com>
2023-10-13 23:41:00 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/exercise4.py
@@ -0,0 +1,6 @@
+# Create a list of people you'd like to invite to dinner
+guest_list = ["abdur rehman", "taha", "basim"]
+
+# Generate invitation messages
+for guest in guest_list:
+    print(f"Dear {guest},\nYou are cordially invited to dinner at my place. It would be an honor to have you join us for an evening of great conversation and delicious food. Please let me know if you can make it.\n\nSincerely, [Your Name]\n")
