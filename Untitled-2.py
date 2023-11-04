e014f13250e4ff145bf747e912621adf9e6d0888
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 17:20:13 +0400

    Create Untitled-2.py

---
+++ Untitled-2.py
@@ -0,0 +1,9 @@
+# Homogeneous List / Python Supports Hetrogeneous List other than C , Java
+
+name =["abdur rahman" , "irtaza " , "basim" ]
+print(name)
+
+#repitition opreator * repeat data 
+
+
+number =[1, 2, 3, 4,]
