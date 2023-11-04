39e22220e010ec5a0368541ce77621743cd2413e
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 18:06:03 +0400

    ch4, ex1

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,7 @@
+# Assigning the value 'green' to the variable alien_color
+alien_color = 'green'
+
+# Testing if the alien's color is green
+if alien_color == 'green':
+    # If the test passes, it prints a message indicating the player earned 5 points.
+    print("You just earned 5 points for shooting the alien!")
