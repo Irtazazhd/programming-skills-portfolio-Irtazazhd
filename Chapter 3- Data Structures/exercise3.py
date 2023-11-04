61ebcb52a4af57bd8ccb69038878353d7e264578
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 17:36:47 +0400

    ch3, ex3

--- Chapter 3- Data Structures/exercise3.py
+++ Chapter 3- Data Structures/exercise3.py
@@ -1,6 +1,5 @@
 # Create a list of favorite modes of transportation
-transportation_list = ["Honda motorcycle", "Tesla Model 3", "Cessna 172 airplane", "Bicycle", "Jeep Wrangler"]
-
+transportation_list = ["supra mk4", "gtr r34", "gtr r35", "patrol vtc", "land cruser station"]
 # Print statements about these items
 for item in transportation_list:
     print(f"I would like to own a {item}.")
