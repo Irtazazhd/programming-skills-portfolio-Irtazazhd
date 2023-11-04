f18550bbaf1f4433941fc0928223613fc78a9c9e
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 19:02:47 +0400

    ch7, ex1

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,8 @@
+def display_message():
+    """Display a message about what I am learning in this chapter."""
+    message = "In this chapter, I am learning about functions in Python."
+    print(message)
+
+# Call the function to display the message
+display_message()
+
