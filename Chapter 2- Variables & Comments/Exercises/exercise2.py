50abe7bae852afe220d11628c07e3b7392bc18d3
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 23:49:32 +0400

    ch2, ex2

--- Chapter 2- Variables & Comments/Exercises/exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -1,6 +1,6 @@
 # Define the quote and its author
 quote = "You must be the change you wish to see in the world."
-author = "Mahatma Gandhi"
+author = "irti"
 
 # Print the quote and its author
-print(f'{author} once said, "{quote}"')
+print(f'{author} once said, "{quote}"')  
--- Chapter 7- Functions/Exercises/exercise5.py
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -1,8 +1,35 @@
-def describe_city(city, country="unspecified"):
-    """Print a sentence describing the city and its country."""
-    print(f"{city} is in {country}.")
 
-# Call the function for three different cities
-describe_city("grand mall", "ajman")
-describe_city("dhaid", "sharjah")
-describe_city("UAE")
+'''
+x = 100
+y = 200 
+z = 300
+print('x' , end = ' = ')
+print('y' , end = ' = ')
+print('z ')
+'''
+'''
+name1  = 'irti'
+name2 = 'abdu'
+name3 = 'topper'
+print('my name is',name1)
+print('my name is',name2)
+print('my name is',name3)
+'''
+'''
+name = 'irti'
+country = 'pakistan'
+age = '16'
+print('my name is', name )
+print('i live in' , country )
+print(f'my age is', age )
+'''
+name = 'irti'
+age = '16'
+country ='pakistan'
+#print(f'my name is', name , 'my age is', age, 'and i live in', country)
+
+name = 'topper'
+age = '18'
+harami = 'infinite'
+print(f'my name is', name, 'my age is', age, 'i am so harami', harami)
+
