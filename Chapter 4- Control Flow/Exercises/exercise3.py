fb692c43fdb381c96aec652c58f228bdc93891ea
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 18:35:46 +0400

    ch4, ex3

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,32 @@
+# Assign the color 'green' to the variable alien_color
+alien_color = 'green'
+
+# If-elif-else chain to test the alien's color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+elif alien_color == 'red':
+    print("You just earned 15 points!")
+
+# Assign the color 'yellow' to the variable alien_color
+alien_color = 'yellow'
+
+# If-elif-else chain to test the alien's color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+elif alien_color == 'red':
+    print("You just earned 15 points!")
+
+# Assign the color 'red' to the variable alien_color
+alien_color = 'red'
+
+# If-elif-else chain to test the alien's color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+elif alien_color == 'red':
+    print("You just earned 15 points!")
