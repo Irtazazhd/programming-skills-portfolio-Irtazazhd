f7d84174819eec57fc174ec814a5c9f0f267c821
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 18:44:30 +0400

    ch5, ex1

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,12 @@
+# Define a dictionary with information about a person
+person_info = {
+    'first_name': 'muhammad',
+    'last_name': 'irtaza',
+    'age': 16,
+    'city': 'sharjah'}
+
+# Print each piece of information stored in the dictionary
+print(f"First Name: {person_info['first_name']}")
+print(f"Last Name: {person_info['last_name']}")
+print(f"Age: {person_info['age']}")
+print(f"City: {person_info['city']}")
