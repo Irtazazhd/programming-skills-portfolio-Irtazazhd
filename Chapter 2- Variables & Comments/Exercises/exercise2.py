7746264fca2a52ee553159a61df17a1d24f5d022
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 23:51:47 +0400

    ch2, ex3

--- Chapter 2- Variables & Comments/Exercises/exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -3,4 +3,4 @@ quote = "You must be the change you wish to see in the world."
 author = "irti"
 
 # Print the quote and its author
-print(f'{author} once said, "{quote}"')  
+print(f'{author} once said, "{quote}"')    
--- Chapter 2- Variables & Comments/Exercises/exercise3.py
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -1,11 +1,11 @@
 # Define a name with leading and trailing whitespace characters
-name = "\t\n   John Doe   \t\n"
+name = "\t\n   irti   \t\n"
 
 # Print the name with surrounding whitespace
 print("Original Name:")
 print(name)
 
-# Strip and print the name using lstrip(), rstrip(), and strip()
+# Strip and print the name using lstrip(), rstrip(), and strip()  
 print("\nAfter Stripping:")
 print("Using lstrip():", name.lstrip())
 print("Using rstrip():", name.rstrip())
