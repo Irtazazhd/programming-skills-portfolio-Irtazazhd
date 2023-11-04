17a0a83449caffd430b4b1d999a905a2e1815451
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 18:28:27 +0400

    ch4, ex2

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,21 @@
+# Assigning the value 'green' to the variable alien_color
+alien_color = 'green'
+
+# If-else chain to test the alien's color
+if alien_color == 'green':
+    # This block runs because alien_color is 'green'.
+    print("You just earned 5 points for shooting the alien!")
+else:
+    # This block will not execute because the if condition is true.
+    print("You just earned 10 points for shooting the alien!")
+
+# Assigning a value other than 'green' to the variable alien_color, let's choose 'red'.
+alien_color = 'red'
+
+# If-else chain to test the alien's color
+if alien_color == 'green':
+    # This block will not execute because alien_color is not 'green'.
+    print("You just earned 5 points for shooting the alien!")
+else:
+    # This block runs because the if condition is False.
+    print("You just earned 10 points for shooting the alien!")
