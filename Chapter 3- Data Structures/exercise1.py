75ca466ad05316518d79d77086703409cd95af07
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 17:32:40 +0400

    chapter3.py
    ch3, ex1

--- Chapter 3- Data Structures/exercise1.py
+++ Chapter 3- Data Structures/exercise1.py
@@ -1,5 +1,5 @@
 # Create a list of friends' names
-names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+names = ["irtaza", "abdu", "taha", "faseeh", "topper"]
 
 # Print each person's name one at a time
 for name in names:
