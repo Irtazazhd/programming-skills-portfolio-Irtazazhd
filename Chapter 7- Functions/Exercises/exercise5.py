6180770594d00c9b0f1e4696715a44fa70bae699
irtaza <muhammadirtaza560@gmail.com>
2023-11-04 19:10:34 +0400

    ch7, ex5

---
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -0,0 +1,8 @@
+def describe_city(city, country="unspecified"):
+    """Print a sentence describing the city and its country."""
+    print(f"{city} is in {country}.")
+
+# Call the function for three different cities
+describe_city("grand mall", "ajman")
+describe_city("dhaid", "sharjah")
+describe_city("UAE")
