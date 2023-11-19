50938f04736572bfdeaf4988c93182218fe7de40
irtaza <muhammadirtaza560@gmail.com>
2023-11-19 23:50:43 +0400

    Update exercise5.py

--- Chapter 7- Functions/Exercises/exercise5.py
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -1,53 +1,10 @@
+def describe_city(city, country="UAE"):
+    print(f"{city} is in {country}.")
 
-'''
-x = 100
-y = 200 
-z = 300
-print('x' , end = ' = ')
-print('y' , end = ' = ')
-print('z ')
-'''
-'''
-name1  = 'irti'
-name2 = 'abdu'
-name3 = 'topper'
-print('my name is',name1)
-print('my name is',name2)
-print('my name is',name3)
-'''
-'''
-name = 'irti'
-country = 'pakistan'
-age = '16'
-print('my name is', name )
-print('i live in' , country )
-print(f'my age is', age )
-'''
+# Calling the function for three different cities
+describe_city("abu dhabi")
+describe_city("dubai", "UAE")
+describe_city("ajman", "UAE")
 
-'''
-name = 'irti'
-age = '16'
-country ='pakistan'
-#print(f'my name is', name , 'my age is', age, 'and i live in', country)
 
-name = 'topper'
-age = '18'
-harami = 'infinite'
-print(f'my name is', name, 'my age is', age, 'i am so harami', harami)
-'''
-'''
-x = 20
-print(type (x))
 
-x = 20.10
-print(type (x))
-
-x = '20'
-print(type (x))
-
-x = 20 + 10j
-print(type (x))
-'''
-
-y = False
-print(type(y))
