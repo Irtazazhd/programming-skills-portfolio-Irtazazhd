eb812755f427e93449339c3f3e245adf30884522
irtaza <muhammadirtaza560@gmail.com>
2023-11-19 23:50:40 +0400

    Update exercise1.py

--- Chapter 4- Control Flow/Exercises/exercise1.py
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -1,14 +1,9 @@
-# Assigning the value 'green' to the variable alien_color
 alien_color = 'green'
 
-# Testing if the alien's color is green
 if alien_color == 'green':
-    # If the test passes, it prints a message indicating the player earned 5 points.
-    print("You just earned 5 points for shooting the alien!")
-# Assigning a value other than 'green' to the variable alien_color
-alien_color = 'yellow'
+    print("Player just earned 5 points.")
+    
+alien_color = 'red'
 
-# Testing if the alien's color is green
 if alien_color == 'green':
-    # This block will not execute because the condition is false.
-    print("You just earned 5 points for shooting the alien!")
+    print("Player just earned 5 points.")
