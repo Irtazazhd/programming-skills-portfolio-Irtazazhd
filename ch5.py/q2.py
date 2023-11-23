41ce7aa2e14918aed9bd98accdd5934724ae89c8
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:51 +0400

    Create q2.py

---
+++ ch5.py/q2.py
@@ -0,0 +1,13 @@
+# Define a dictionary
+my_dict = {
+    "name": "irti",
+    "age": 16,
+    "location": "sharjahh",
+    "occupation": "student",
+    "hobbies": ["football", "wallyball", "gym"],
+    "email": "muhammadirtaza560@gmail.com"
+}
+
+# Iterate through the keys of the dictionary and print them
+for key in my_dict.keys():
+    print(key)
