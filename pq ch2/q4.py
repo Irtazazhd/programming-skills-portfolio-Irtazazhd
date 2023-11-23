77dc9ccdaf9b239904273c30fbdca29bdbd52434
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:34:17 +0400

    Create q4.py

---
+++ pq ch2/q4.py
@@ -0,0 +1,7 @@
+# Storing an integer and a string value in variables x and y
+x = 10       # Example integer
+y = "Hello"  # Example string
+
+# Printing the types of x and y
+print("Type of x:", type(x))
+print("Type of y:", type(y))
