ecf5496eb27fdfabbbe1db08e81d1dff5f01ca6a
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:34:20 +0400

    Create q5.py

---
+++ pq ch2/q5.py
@@ -0,0 +1,10 @@
+# Creating a list of fruit names
+fruits = ["Apple", "Banana", "Cherry"]
+
+# Unpacking the list into three variables
+x, y, z = fruits
+
+# Printing the values of each variable
+print("x:", x)
+print("y:", y)
+print("z:", z)
