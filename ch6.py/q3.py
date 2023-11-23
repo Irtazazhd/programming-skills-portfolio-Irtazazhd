f0dd563764f7ec577460e5ce9f5be9c657431af6
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:32:07 +0400

    Create q3.py

---
+++ ch6.py/q3.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+total_sum = 0
+
+# Use a for loop to iterate through numbers from 1 to 100
+for number in range(1, 101):
+    total_sum += number
+
+# Print the sum
+print("The sum of numbers from 1 to 100 is:", total_sum)
