5d65184b0adf9b57ba9d605fdf3686eff7987e11
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:21 +0400

    Create q1.py

---
+++ ch3.py/q1.py
@@ -0,0 +1,8 @@
+# Given list
+list1 = [5, 20, 15, 20, 25, 50, 20]
+
+# Removing all occurrences of item 20 using list comprehension
+list1 = [item for item in list1 if item != 20]
+
+# Printing the modified list
+print("Modified list:", list1)
