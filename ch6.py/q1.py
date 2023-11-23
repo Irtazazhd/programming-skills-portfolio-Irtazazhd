8b9eeacaac4ab5567be10ba7b01e585f08961126
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:32:01 +0400

    Create q1.py

---
+++ ch6.py/q1.py
@@ -0,0 +1,7 @@
+# Initialize a variable to start with 2
+number = 2
+
+# Use a while loop to print even numbers from 2 to 10
+while number <= 10:
+    print(number)
+    number += 2  # Increment the number by 2 to get the next even number
