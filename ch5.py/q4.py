7735847550a4f6ef0a8e223844a43bbefd927b86
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:56 +0400

    Create q4.py

---
+++ ch5.py/q4.py
@@ -0,0 +1,16 @@
+# Define a dictionary
+my_dict = {
+    "name": "irti",
+    "age": 16,
+    "location": "sharjahh",
+    "occupation": "student",
+    "hobbies": ["football", "wallyball", "gym"],
+    "email": "muhammadirtaza560@gmail.com"
+}
+# Iterate through both keys and values of the dictionary and print them
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
+
+# Iterate through both keys and values of the dictionary and print them
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
