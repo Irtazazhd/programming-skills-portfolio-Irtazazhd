164a4ea052ca8234c3b1d77ca6d13c0e433ef8ed
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:24 +0400

    Create q2.py

---
+++ ch3.py/q2.py
@@ -0,0 +1,7 @@
+# Create a list with the names of famous scientists
+famous_scientists = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+
+# Print the list
+print("List of Famous Scientists:")
+for scientist in famous_scientists:
+    print(scientist)
