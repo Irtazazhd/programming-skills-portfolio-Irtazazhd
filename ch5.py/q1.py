2bc76f9bcf0c46a4842358f91058bd69f7df65c9
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:49 +0400

    Create q1.py

---
+++ ch5.py/q1.py
@@ -0,0 +1,17 @@
+# Create a dictionary to store information about yourself
+my_info = {
+    "name": "irti",
+    "age": 16,
+    "location": "sharjahh",
+    "occupation": "student",
+    "hobbies": ["football", "wallyball", "gym"],
+    "email": "muhammadirtaza560@gmail.com"
+}
+
+# Access and print information from the dictionary
+print("Name:", my_info["name"])
+print("Age:", my_info["age"])
+print("Location:", my_info["location"])
+print("Occupation:", my_info["occupation"])
+print("Hobbies:", ", ".join(my_info["hobbies"]))
+print("Email:", my_info["email"])
