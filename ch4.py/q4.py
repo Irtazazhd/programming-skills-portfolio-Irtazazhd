db7371d566e794f3c71a1a2e6986de3ffdf9cac2
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:43 +0400

    Create q4.py

---
+++ ch4.py/q4.py
@@ -0,0 +1,9 @@
+# Define the speed variable
+speed = 40  # Replace this with the actual speed value you want to check
+
+# Check if the speed is within the specified range
+if 24 <= speed <= 56:
+    print("Speed is normal")
+else:
+    print("Speed is abnormal")
+
