a25e710a140f83fee5f28f54f309568fc33ba868
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:31:35 +0400

    Create q1.py

---
+++ ch4.py/q1.py
@@ -0,0 +1,14 @@
+# Assuming 'some_variable' is a variable that holds a numeric value
+some_variable = 120  # You can replace this with the actual value of your variable
+
+if some_variable > 100:
+    y = 20
+    z = 40
+else:
+    # Assign some other values to y and z if some_variable is not greater than 100
+    y = 0
+    z = 0
+
+# Printing the values of y and z
+print("y =", y)
+print("z =", z)
