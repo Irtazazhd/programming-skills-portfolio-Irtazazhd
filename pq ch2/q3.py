76a0dc2ac2b1f53e1d4e79decaaaa286b73ace01
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:34:15 +0400

    Create q3.py

---
+++ pq ch2/q3.py
@@ -0,0 +1,12 @@
+# Prompting the user to enter the number 5
+user_input = input("Please enter the number 5: ")
+
+# Type casting the input into different data types
+string_version = str(user_input)
+int_version = int(user_input)
+float_version = float(user_input)
+
+# Printing the different type-casted versions of the input
+print("String version:", string_version)
+print("Integer version:", int_version)
+print("Float version:", float_version)
