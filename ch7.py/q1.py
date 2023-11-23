1977d3de5acebf4b74093664d581565d428059e3
irtaza <muhammadirtaza560@gmail.com>
2023-11-24 00:33:31 +0400

    Create q1.py

---
+++ ch7.py/q1.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    return a + b
+
+# Example usage:
+num1 = 5  # Set num1 to 5
+num2 = 8  # Set num2 to 8
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}")
